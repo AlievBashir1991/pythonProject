3798841ba3fb139c2acf681f1553cfd2c58187ee
AlievBashir1991 <alievbasir185@gmail.com>
2024-12-07 01:59:03 +0500

    Add files via upload
    15.0
    True
    6
    8
    False
    4

---
+++ main_v1A97C9.py
@@ -0,0 +1,9 @@
+print(9 ** 0.5 * 5)                             #"1st program".
+
+print(9.99 > 9.98 != 1000 != 1000.1)            #"2st program".
+
+print(2 * 2 + 2)                                #"3st program".
+print(2 * (2 + 2))                              #"3st program".
+print(2 * 2 + 2 == 2 * (2 + 2))                 #"3st program".
+
+                          # "4st program".
